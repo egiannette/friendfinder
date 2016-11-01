5fb979e827ec68d89ac54a34dc68a0056e7b929f
Erin Giannette <emg4784@yahoo.com>
2016-11-01 17:00:46 -0500

    adding js

---
+++ data/friends.js
@@ -0,0 +1,67 @@
+// friend data
+
+var friends = [{
+	"routeName": 'Betty',
+	"name": 'Betty',
+	"picture":'https:www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj_voXZr4bQAhUCQiYKHRdfAKcQjRwIBw&url=https%3A%2F%2Ftwitter.com%2Fbettyfckinwhite&psig=AFQjCNHNfsMEoP_pjjLd6d-lly9vxoVXWw&ust=1478048981356302',
+	"scores":[
+	1,
+	3,
+	4,
+	3,
+	4,
+	3,
+	2,
+	1,
+	1,
+	2
+	]
+},{
+	"routeName": 'Sophia',
+	"name": 'Sophia',
+	"picture":'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjVw_izsobQAhWFJCYKHeroA3MQjRwIBw&url=http%3A%2F%2Fquotesgram.com%2Fgolden-girls-sophia-quotes%2F&psig=AFQjCNEI6KwAOcnBFA-UX5a2pW4u1gJu6w&ust=1478049702065677',
+	"scores":[
+	3,
+	3,
+	4,
+	4,
+	4,
+	1,
+	1,
+	1,
+	2,
+	3
+	]
+},{
+	"routeName": 'Blanche',
+	"name": 'Blanche',
+	"picture":'https:www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj6r6qos4bQAhUGRyYKHanhCWwQjRwIBw&url=http%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fentertainment%2Fg2758%2Fgolden-girls-cast-young%2F&psig=AFQjCNGyjUjlElolrumpxuqHanO-5FFyIA&ust=1478049908428883',
+	"scores":[
+	1,
+	1,
+	1,
+	1,
+	2,
+	2,
+	2,
+	3,
+	3,
+	3
+	]
+},{
+	"routeName":'Dorothy',
+	"name":'Dorothy',
+	"picture":"https:www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiav7uztIbQAhWBVSYKHU82BvUQjRwIBw&url=http%3A%2F%2Fwww.the90swereawesome.com%2Fthe-golden-girls-facts%2F&psig=AFQjCNHjuzcoxNlv038AOMagnrakfulg4A&ust=1478050201387421",
+	"scores":[
+	5,
+	5,
+	5,
+	5,
+	5,
+	4,
+	4,
+	4,
+	4,
+	4
+	]
+}];
---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Sets up the Express App
+
+var app = express();
+var PORT = process.env.PORT||8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+
+// Create New Characters - takes in JSON input
+app.post('/api/new', function (req, res) {
+	var friends = req.body;
+	friends.routeName = friends.name.replace(/\s+/g, '').toLowerCase();
+
+	console.log(newcharacter);
+
+	characters.push(newcharacter);
+
+	res.json(newcharacter);
+});
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+	console.log('App listening on PORT ' + PORT);
+};
